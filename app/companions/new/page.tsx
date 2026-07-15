889786fb9662f841f76edb2dc874dc0b0c8b56f6
Thanh Vỵ <buivy22122004@gmail.com>
2026-07-15 18:24:53 +0700

    first commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,6 @@
+const NewCompanion = () => {
+    return (
+        <div>New Companion form</div>
+    )
+}
+export default NewCompanion
---
+++ app/companions/page.tsx
@@ -0,0 +1,6 @@
+const Page = () => {
+    return (
+        <div>CompanionsLibrary</div>
+    )
+}
+export default Page
